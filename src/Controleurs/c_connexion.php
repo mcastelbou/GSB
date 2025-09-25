3d182a4cdeb8023f01515fdfc23a7719cbe18e49
mcastelbou <martin.castelbou@gmail.com>
2025-09-25 10:49:44 +0200

    Début de généralisation des moyens de connexion aux visiteur et comptables.

--- src/Controleurs/c_connexion.php
+++ src/Controleurs/c_connexion.php
@@ -29,15 +29,15 @@ switch ($action) {
     case 'valideConnexion':
         $login = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $mdp = filter_input(INPUT_POST, 'mdp', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $visiteur = $pdo->getInfosVisiteur($login, $mdp);
-        if (!is_array($visiteur)) {
+        $user = $pdo->getInfosUtilisateur($login, $mdp);
+        if (!is_array($user)) {
             Utilitaires::ajouterErreur('Login ou mot de passe incorrect');
             include PATH_VIEWS . 'v_erreurs.php';
             include PATH_VIEWS . 'v_connexion.php';
         } else {
-            $id = $visiteur['id'];
-            $nom = $visiteur['nom'];
-            $prenom = $visiteur['prenom'];
+            $id = $user['id'];
+            $nom = $user['nom'];
+            $prenom = $user['prenom'];
             Utilitaires::connecter($id, $nom, $prenom);
             header('Location: index.php');
         }
