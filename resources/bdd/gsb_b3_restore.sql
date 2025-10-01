15f18b09c22159e386ac01db92917693f7575b8a
mcastelbou <martin.castelbou@gmail.com>
2025-10-01 15:27:05 +0200

    Maj du restore

--- resources/bdd/gsb_b3_restore.sql
+++ resources/bdd/gsb_b3_restore.sql
@@ -1,14 +1,14 @@
 -- Script de restauration de l'application "GSB Frais"
 
 -- Administration de la base de données
-CREATE DATABASE IF NOT EXISTS gsb_frais 
+CREATE DATABASE IF NOT EXISTS gsb_b3 
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 CREATE USER IF NOT EXISTS 'userGsb'@'localhost' IDENTIFIED BY 'secret';
 GRANT SHOW DATABASES ON *.* TO 'userGsb'@'localhost';
 GRANT ALL PRIVILEGES ON `gsb_frais`.* TO userGsb@localhost;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-USE gsb_frais ;
+USE gsb_b3 ;
 
 -- Création de la structure de la base de données
 CREATE TABLE IF NOT EXISTS fraisforfait (
